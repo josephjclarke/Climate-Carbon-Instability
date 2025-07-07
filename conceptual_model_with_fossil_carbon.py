40bd76db1a10d2027c55f8665d5dbd0c81274873
Joe Clarke <j.j.clarke@exeter.ac.uk>
2025-07-07 16:10:49 +0100

    removed unused code

--- conceptual_model_with_fossil_carbon.py
+++ conceptual_model_with_fossil_carbon.py
@@ -7,7 +7,6 @@ import scipy.interpolate
 import scipy.optimize
 import dataclasses
 import matplotlib.pyplot as plt
-import sdeint
 
 plt.rcParams["font.family"] = "serif"
 
@@ -22,28 +21,28 @@ class ClimateCarbonSystem:
 
     Ca0: float = 286.085 * 2.12
     CL0: float = 1630
-    V1V2ratio: float = 1 / 50.0  # 1/48.6 #1 / 58.0  # 1 / 50.0
-    nu1: float = 0.2  # 0.47 #0.2 #1 / 20.0 # 0.2
-    nu2: float = 1 / 500.0  # 0.05
+    V1V2ratio: float = 1 / 50.0
+    nu1: float = 0.2
+    nu2: float = 1 / 500.0
     kappa: float = 0.57
     Q2x: float = 3.7
     Gamma1: float = 0.13
     Gamma2: float = 0.01
-    npp0: float = 65.0  # 50.0
-    Ca05: float = 344 * 2.12  # 280 * 2.12
+    npp0: float = 65.0
+    Ca05: float = 344 * 2.12
     Q10: float = 2.0
     L: float = 1.0
     t_final: float = 1e6
     AM: float = 1.77e20
     OM: float = 7.8e22
-    Alk: float = 5130  # 767.0  # 9.57E+02 # 4810.0  # 767.0
-    k1: float = 1e-6  # 8e-7  # 7.53E-07 # 7.92e-7  # 8e-7
-    k2: float = 7.53e-10  # 4.53e-10  # 4.43e-10  #6.69E-10 # 5.12e-10  # 4.53e-10
-    kH0: float = 1.91e3 * 0.995268992324945  # 1.23e3  # * 1.215 #1.06
+    Alk: float = 5130
+    k1: float = 1e-6
+    k2: float = 7.53e-10
+    kH0: float = 1.91e3 * 0.995268992324945
     linear_ocean: bool = False
     burnt_fossil_carbon: float = 0.0
-    quadratic_feedback: float = 0.0  # -0.03
-    cubic_feedback: float = 0.0  # 0.00025
+    quadratic_feedback: float = 0.0
+    cubic_feedback: float = 0.0
     land_global_warming_ratio: float = 1.3  # warming over land / warming globally
     sigma_Q: float = 0.5
 
@@ -65,12 +64,7 @@ class ClimateCarbonSystem:
             * np.sqrt(self.Ca0 * self.k1 + 8 * A * self.Alk * self.k2)
             - self.Ca0 * (self.k1 - 8 * self.k2)
         ) / (8 * A * self.k2)
-        # self.C10 = scipy.optimize.fsolve(
-        #     lambda C1: self.Ca0 - self.calc_A() * self.calc_B(C1) * C1, x0=1000.0
-        # ).item()
         self.C20 = self.C10 / self.V1V2ratio
-        # self.C10 = (self.Ca0 / 7.48871623e-08) ** (1 / 3.13855535e+00)
-        # self.C20 = self.C10 / self.V1V2ratio
         self.pi_equilibrium = np.array([0.0, 0.0, self.CL0, self.C10, self.C20])
         dCL = -100.0
         dC1 = -100.0
@@ -192,7 +186,6 @@ class ClimateCarbonSystem:
             A.
 
         """
-        # return 7.48871623e-08
         return self.HenryCoeff(T) * self.AM / (self.OM / (1 + 1 / self.V1V2ratio))
 
     def calc_B(self, C1):
@@ -210,14 +203,7 @@ class ClimateCarbonSystem:
             B parameter.
 
         """
-        # return C1 ** (3.13855535e00 - 1)
         c = np.full_like(C1, self.C10) if self.linear_ocean else C1
-
-        # p0 = 1
-        # p1 = self.k1 - C1 * self.k1 / self.Alk
-        # p2 = (1 - 2 * C1 / self.Alk) * self.k1 * self.k2
-        # h = 1 / (np.roots([p2, p1, p0])).max()
-        # return 1 / (1 + self.k1 / h + self.k1 * self.k2 / h**2)
 
         B_b = (-self.Alk + c) * self.k1 + 4 * (self.Alk - 2 * c) * self.k2
         B_disc = (self.k1 * (self.Alk - c)) ** 2 - 4 * self.Alk * self.k1 * self.k2 * (
@@ -240,9 +226,6 @@ class ClimateCarbonSystem:
             derivative.
 
         """
-        # return 2.13855535e+00 * C1 ** (3.13855535e+00 - 2)
-
-        # return nd.Derivative(self.calc_B(C1))
 
         if self.linear_ocean:
             return np.zeros_like(C1)
@@ -550,7 +533,7 @@ class ClimateCarbonSystem:
 
     def analytic_growth_rate(self, L):
         """
-        Calculates the growth rate near the bifurcation point.
+        Calculate the growth rate near the bifurcation point.
 
         The growth rate is given by (lambda + g_1) / (g_2 lambda + g_3).
 
@@ -735,214 +718,3 @@ class ClimateCarbonSystem:
         g_2 = r_3 / r_1
         g_3 = r_4 / r_1
         return (L + g_1) / (g_2 * L + g_3)
-
-    def solve_sde(self, tspan=np.arange(10_000)):
-        g = np.zeros((5, 5))
-        g[0, 0] = self.sigma_Q
-
-        f = lambda y, t: self.dFdt(t, y)
-        G = lambda y, t: g
-        self.tspan = tspan
-        self.result = sdeint.itoint(f, G, self.pi_equilibrium, tspan)
-
-
-def find_critical_lambda(
-    varname: str,
-    vals: npt.ArrayLike,
-    tol: float = 1e-5,
-    other_params: dict = {},
-    detail: float = 2,
-):
-    """
-    Calculate the critical lambda value as parameter is varied.
-
-    A bisection method is used.
-
-    Parameters
-    ----------
-    varname : str
-        Name of parameter to vary.
-    vals : npt.ArrayLike
-        parameter values to get critical lambda at.
-    tol : float, optional
-        Numerical tolerance to calculate critical lambdas.
-        The default is 1e-5.
-    other_params: dict
-        Other parameters that can be changed from default.
-    detail : float
-        How finely to set upper and lower bounds
-
-    Returns
-    -------
-    critical_Ls : array_like
-        Critical Lambda Values.
-    critical_ECS : array_like
-        Corresponding ECS values.
-
-    """
-    critical_Ls = np.empty_like(vals, "float")
-    critical_ECS = np.empty_like(vals, "float")
-    for idx, v in np.ndenumerate(vals):
-        test_Ls = np.geomspace(1e-3, 2.0, detail)
-        stabs = np.zeros_like(test_Ls)
-        for ind, L in np.ndenumerate(test_Ls):
-            ccs = ClimateCarbonSystem(L=L, **{varname: v}, **other_params)
-            ccs.find_equilibrium()
-            stabs[ind] = ccs.jac_eig_at(ccs.equilibrium).real.max()
-
-        stab_boundary = np.argmax(np.sign(stabs[::-1]))
-        L_lb = test_Ls[::-1][stab_boundary]
-        L_ub = test_Ls[::-1][stab_boundary - 1]
-        L_ce = 0.5 * (L_lb + L_ub)
-        while (L_ub - L_lb) / L_ce > tol:
-            ccs = ClimateCarbonSystem(L=L_ce, **{varname: v}, **other_params)
-            ccs.find_equilibrium()
-            max_eval = ccs.jac_eig_at(ccs.equilibrium).real.max()
-            if max_eval > 0:
-                L_lb = L_ce
-            else:
-                L_ub = L_ce
-            L_ce = 0.5 * (L_lb + L_ub)
-        critical_Ls[idx] = L_ce
-        critical_ECS[idx] = ccs.Q2x / L_ce
-    return critical_Ls, critical_ECS
-
-
-def find_critical_ECS_two_parameter(
-    varname1: str,
-    varname2: str,
-    val1: npt.ArrayLike,
-    val2: npt.ArrayLike,
-    tol: float = 1e-5,
-    other_params: dict = {},
-    detail: float = 2,
-) -> npt.ArrayLike:
-    """
-    Calculate, by bisection, the critical ECS as two parameters are varied.
-
-    Parameters
-    ----------
-    varname1 : str
-        Name of 1st variable, shape n.
-    varname2 : str
-        Name of 2nd variable, shape m.
-    val1 : npt.ArrayLike
-        Values of varname1.
-    val2 : npt.ArrayLike
-        Values of varname2.
-    tol : float, optional
-        Numerical tolerence of critical sensitivities. The default is 1e-5.
-    other_params : dict, optional
-        Parameters that are set to non-default values. The default is {}.
-    detail : float, optional
-        How finely to set the upper and lower bounds. The default is 2.
-
-    Returns
-    -------
-    ECS_crit : npt.ArrayLike
-        Critical ECS values shape (n,m).
-
-    """
-    v1s, v2s = np.meshgrid(val1, val2)
-    ECS_crit = np.zeros_like(v1s)
-    for idx in np.ndindex(ECS_crit.shape):
-        v1 = v1s[idx]
-        v2 = v2s[idx]
-        _, ec = find_critical_lambda(
-            varname=varname1,
-            vals=v1,
-            other_params=other_params | {varname2: v2},
-            tol=tol,
-            detail=detail,
-        )
-        ECS_crit[idx] = ec.item()
-    return ECS_crit
-
-
-def find_critial_bfc(Ls: npt.ArrayLike, other_params: dict = {}):
-    """
-    Calculate the critical cumulative emissions that leads to an instability.
-
-    This is a function of climate sensitivity.
-
-    Parameters
-    ----------
-    Ls : npt.ArrayLike
-        Climate Sensitivities.
-    other_params : dict, optional
-        Other parameters to pass to ClimateCarbonSystems. The default is {}.
-
-    Returns
-    -------
-    bfc_c : array_like
-        Critical Cumulative Emissions.
-
-    """
-    bfc_c = np.full_like(Ls, np.nan, float)
-    for idx, L in np.ndenumerate(Ls):
-        initial_ccs = ClimateCarbonSystem(L=L, **other_params)
-        eq = initial_ccs.pi_equilibrium
-        unstable_eig = initial_ccs.jac_eig_at(eq).real.max()
-        if unstable_eig > 0:
-            bfc_c[idx] = 0.0
-        for C in np.linspace(0.0, -1e3, int(1e3 + 1)):
-            ccs = ClimateCarbonSystem(L=L, burnt_fossil_carbon=C, **other_params)
-            ccs.find_equilibrium(eq)
-            eq = ccs.equilibrium
-            unstable_eig = ccs.jac_eig_at(eq).real.max()
-            if unstable_eig > 0:
-                bfc_c[idx] = C
-                break
-    return bfc_c
-
-
-def plot_system(ccs):
-    """
-    Plot the perturbed climate carbon system.
-
-    Parameters
-    ----------
-    ccs : ClimateCarbonSystem object
-        the climate carbon system.
-
-    Returns
-    -------
-    fig : matplotlib figure
-        figure object used in plotting.
-
-    """
-    time = ccs.t
-    T1 = ccs.T1
-    T2 = ccs.T2
-    CL = ccs.CL
-    C1 = ccs.C1
-    C2 = ccs.C2
-    Ca = ccs.Ca
-
-    fig, axs = plt.subplots(nrows=5, ncols=1, sharex=True, figsize=(6.4, 10.0))
-
-    axs[0].plot(time, Ca, color="black")
-    axs[0].set_ylabel("CO2 PgC")
-    axs[0].axhline(ccs.Ca0, linestyle=":", color="black")
-
-    axs[1].plot(time, T1, label=r"$T_1$", color="#003f5c")
-    axs[1].plot(time, T2, label=r"$T_2$", color="#ffa600")
-    axs[1].set_ylabel("Temperature K")
-    axs[1].axhline(0.0, linestyle=":", color="black")
-    axs[1].legend(frameon=False)
-
-    axs[2].plot(time, CL, color="black")
-    axs[2].set_ylabel("Soil Carbon PgC")
-    axs[2].axhline(ccs.CL0, linestyle=":", color="black")
-
-    axs[3].plot(time, C1, label=r"$C_1$", color="black")
-    axs[3].set_ylabel("Upper Ocean Carbon PgC")
-    axs[3].axhline(ccs.C10, linestyle=":", color="black")
-
-    axs[4].plot(time, C2, label=r"$C_2$", color="black")
-    axs[4].set_ylabel("Lower Ocean Carbon PgC")
-    axs[4].axhline(ccs.C20, linestyle=":", color="black")
-
-    fig.supxlabel("Time yr")
-    fig.suptitle(f"Climate-Carbon System with ECS = {ccs.Q2x/ccs.L}K")
-    return fig
